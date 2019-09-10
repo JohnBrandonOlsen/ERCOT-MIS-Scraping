1736ce0187f9ff44bc5121304b8c70cfadd7dd02
JohnBrandonOlsen <47157463+JohnBrandonOlsen@users.noreply.github.com>
2019-09-10 13:48:33 -0400

    Add files via upload
    Reworked to eliminate multiple unnecessary global statements and follow more closely to "Pythonic" standards. Special thanks to Reddit user /u/CrambleSquash for guidance and encouragement.

--- downloadSPP.py
+++ downloadSPP.py
@@ -12,11 +12,80 @@ from os import remove
 from bs4 import BeautifulSoup
 import xlwings as xw
 
-downloads_folder = "path/to/downloads_folder"
-save_folder = "path/to/save_folder"
+downloads_folder = "C:/Users/END Team/Desktop/New folder/Dropbox (ISS LLC)/PPA/Brandon/LMP Evaluation/ERCOT/ERCOT Temp/"
+save_folder = "C:/Users/END Team/Desktop/New folder/Dropbox (ISS LLC)/PPA/Brandon/LMP Evaluation/ERCOT/"
 ercot_site_address = "http://mis.ercot.com/misapp/GetReports.do?reportTypeId=12301&reportTitle=Settlement%20Point%20Prices%20at%20Resource%20Nodes,%20Hubs%20and%20Load%20Zones&showHTMLView=&mimicKey"
 
-def create_new_daily_xlsx():
+def generate_documents_list(site_address):
+    try:
+        res = requests.get(site_address)
+        ercot_soup = BeautifulSoup(res.text, "lxml")
+        list_of_documents = ercot_soup.findAll('td', attrs={'class': 'labelOptional_ind'})
+        list_of_links = ercot_soup.select('a')
+
+        return list_of_documents, list_of_links
+
+    except:
+        print("\nERCOT MIS Site Failure at: " + time.ctime())
+        time.sleep(300)
+
+def initial_most_recent_download(top_document):
+    try:
+        last_save = open("testSPP.txt", "r")
+        most_recent_download = last_save.read()
+        last_save.close()
+    except:
+        print("No previous save file exists! Creating new file.")
+        last_save = open("testSPP.txt", "w")
+        last_save.write(top_document)
+        last_save.close()
+        most_recent_download = top_document
+        pass
+
+    return most_recent_download
+
+def find_document_iterable(most_recent_download, list_of_documents):
+    document_iterable = 0
+    temp_download = list_of_documents[document_iterable].text[62:70] + list_of_documents[document_iterable].text[71:75]
+
+    while int(temp_download) != int(most_recent_download):
+        document_iterable = document_iterable + 2   #Because the page listsl csv and xml documents, needs to cycle two at a time
+        temp_download = list_of_documents[document_iterable].text[62:70] + list_of_documents[document_iterable].text[71:75]
+    if document_iterable != 0:
+        document_iterable -= 2
+    else:
+        pass
+
+    return document_iterable
+
+def download_csv_zip(most_recent_download, list_of_documents, list_of_links):
+    current_document_position = find_document_iterable(most_recent_download, list_of_documents)
+    temp_download = list_of_links[current_document_position].get('href')    #Sets temp_download variable to one instance after most_recent_download variable
+    print("Downloading " + list_of_documents[current_document_position].text[64:] + " at: " + time.ctime(), end='\r')
+    webbrowser.open('http://mis.ercot.com' + str(temp_download))    #Downloads next SPP
+
+    most_recent_download = list_of_documents[current_document_position].text[62:70] + list_of_documents[current_document_position].text[71:75]
+    update_last_save(most_recent_download)
+
+    return(most_recent_download, current_document_position)
+
+
+def update_last_save(most_recent_download):
+        last_save = open("testSPP.txt", "w")
+        last_save.write(most_recent_download)    #Update testSPP with new most_recent_download variable
+        last_save.close()
+
+def verify_download(zip_filename):
+    while True:
+        try:
+            with zf(downloads_folder + zip_filename) as current_zip:
+                current_zip.close()
+                break
+        except:
+            time.sleep(1)
+            continue
+
+def create_new_daily_xlsx(zip_filename, csv_filename, csv_compendium_filename):
     global excel_file
 
     with zf(downloads_folder + zip_filename) as current_zip:
@@ -48,10 +117,7 @@ def create_new_daily_xlsx():
 
     excel_file.save(save_folder + csv_compendium_filename)
 
-def add_price_data():
-    global price_data_loc
-    global price_data_compendium_interval
-
+def add_price_data(zip_filename, csv_filename, price_data_loc):
     with zf(downloads_folder + zip_filename) as current_zip:
         with current_zip.open(csv_filename, 'r') as f:
             readCSV = csv.reader(TextIOWrapper(f), delimiter=',')
@@ -60,64 +126,28 @@ def add_price_data():
             for row in readCSV:
                 price_data.append(row[5])
             excel_file.sheets[0].range(price_data_loc).value = price_data
-            price_data_compendium_interval += 1
-            price_data_loc = "D" + str(price_data_compendium_interval)
         excel_file.save()
     remove(downloads_folder + zip_filename)
 
-def update_csv_data():
-    global zip_filename
-    global csv_filename
-    global csv_compendium_filename
-    global price_data_loc
-    global price_data_compendium_interval
-    global excel_file
-
-    list_of_csvs = ercot_soup.findAll('td', attrs={'class': 'labelOptional_ind'})
-    zip_filename = list_of_csvs[document_iterable].text
-
-    verify_download()
-
-    csv_filename = zf(downloads_folder + zip_filename, 'r').namelist()[0]
-
+def update_csv_data(zip_filename, csv_filename):
     if zip_filename[71:75] == "0015":
         csv_compendium_filename = zip_filename[62:70]+".xlsx"
-        create_new_daily_xlsx()
-        print("File " + csv_compendium_filename + " created.")
-
-        price_data_compendium_interval = 2
-        price_data_loc = "D" + str(price_data_compendium_interval)
-        add_price_data()
+        create_new_daily_xlsx(zip_filename, csv_filename, csv_compendium_filename)
+        print("\nFile " + csv_compendium_filename + " created.")
+        price_data_loc = "D2"
+        add_price_data(zip_filename, csv_filename, price_data_loc)
 
     elif zip_filename[71:75] == "0000":
-        add_price_data()
+        price_data_compendium_interval = "D97"
+        add_price_data(zip_filename, csv_filename, price_data_compendium_interval)
         excel_file.close()
 
     else:
-        try:
-            add_price_data()
-        except:
-            try:
-                csv_compendium_filename = zip_filename[62:70]+".xlsx"
-                excel_file = xw.Book(save_folder + csv_compendium_filename)
+        price_data_loc = "D" + str(find_hour_interval(zip_filename) + 2)
+        add_price_data(zip_filename, csv_filename, price_data_loc)
 
-                price_data_compendium_interval = find_hour_interval() + 1
-                price_data_loc = "D" + str(price_data_compendium_interval)
-                add_price_data()
-            except Exception as e:
-                find_most_recent_0000()
-
-def verify_download():
-    while True:
-        try:
-            with zf(downloads_folder + zip_filename) as current_zip:
-                break
-        except:
-            time.sleep(1)
-            continue
-
-def find_hour_interval():
-    hour_intervals = ["0000", "0015", "0030", "0045", "0100", "0115", "0130",
+def find_hour_interval(zip):
+    hour_intervals = ["0015", "0030", "0045", "0100", "0115", "0130",
                     "0145", "0200", "0215", "0230", "0245", "0300", "0315",
                     "0330", "0345", "0400", "0415", "0430", "0445", "0500",
                     "0515", "0530", "0545", "0600", "0615", "0630", "0645",
@@ -130,61 +160,52 @@ def find_hour_interval():
                     "1730", "1745", "1800", "1815", "1830", "1845", "1900",
                     "1915", "1930", "1945", "2000", "2015", "2030", "2045",
                     "2100", "2115", "2130", "2145", "2200", "2215", "2230",
-                    "2245", "2300", "2315", "2330", "2345"
+                    "2245", "2300", "2315", "2330", "2345", "0000"
                     ]
 
-    position = hour_intervals.index(zip_filename[71:75])
+    position = hour_intervals.index(zip[71:75])
 
     return position
 
-def find_most_recent_0000():
-    global most_recent_download
+def find_most_recent_0000(most_recent_download, list_of_documents, current_document_position, zip_filename):
+    current_document_position = current_document_position + ((find_hour_interval(zip_filename) + 1) * 2)
+    most_recent_download = list_of_documents[current_document_position].text[62:70] + list_of_documents[current_document_position].text[71:75]
+    return most_recent_download
 
-    most_recent_download = list_of_documents[document_iterable + (find_hour_interval() * 2)].get('href')
+def main():
+    while True:
+        ercot_lists = generate_documents_list(ercot_site_address)
+        list_of_documents = ercot_lists[0]
+        list_of_links = ercot_lists[1]
+        top_document = list_of_documents[0].text[62:70] + list_of_documents[0].text[71:75]
+        most_recent_download = initial_most_recent_download(top_document)
 
-while True: #Loops forever until manually interrupted
-    try:
-        #Capturing the reports page in a variable
-        res = requests.get(ercot_site_address)
-        ercot_soup = BeautifulSoup(res.text, "lxml")   #Parsing the HTML from reports page
-        list_of_documents = ercot_soup.select('a')   #Pulls the URL links for active reports from the HTML
-        top_document = list_of_documents[0].get('href')   #Grabs and labels the top active report link
-    except Exception as e:
-        print("ERCOT MIS Site Failure " + time.ctime())
-        time.sleep(300)
+        csv_zip_details = download_csv_zip(most_recent_download, list_of_documents, list_of_links)
 
-    try:
-        last_save = open("testSPP.txt", "r") #Looks for testSPP.txt
-        most_recent_download = last_save.read() #If available, reads testSPP.txt and commits to most_recent_download variable
-        last_save.close()
-    except: #If testSPP.txt does not exist, run the following.
-        print("No previous save file exists! Creating new file.")
-        last_save = open("testSPP.txt", "w") #Creates testSPP.txt
-        last_save.write(top_document)   #Adds the top_Document to the testSPP file
-        last_save.close()
-        most_recent_download = top_document  #Sets most_recent_download variable to the top_Document string
-        pass
+        while int(top_document) != int(most_recent_download):
+            most_recent_download = csv_zip_details[0]
+            current_document_position = csv_zip_details[1]
 
-    #Find the most recent downloaded SPP
-    while (int(top_document[58:]) != int(most_recent_download[58:])):  #document_iterable if top_Document is same as most_recent_download variable
-        document_iterable = 0 #Set a document_iterable value to 0
-        temp_download = list_of_documents[document_iterable].get('href')    #Creates temp_download variable
+            zip_filename = list_of_documents[current_document_position].text
+            verify_download(zip_filename)
+            csv_filename = zf(downloads_folder + zip_filename, 'r').namelist()[0]
 
-        while (int(temp_download[58:]) != int(most_recent_download[58:])):   #Loop until temp_download varable finds most_recent_download variable
-            document_iterable = document_iterable + 2   #Change document_iterable value by 2 (csv files are separated by 2 on page)
-            temp_download = list_of_documents[document_iterable].get('href')    #Resets temp_download variable to next URL
+            try:
+                update_csv_data(zip_filename, csv_filename)
 
-        document_iterable = document_iterable - 2   #Returns document_iterable value to one instance after most_recent_download variable
-        temp_download = list_of_documents[document_iterable].get('href')    #Sets temp_download variable to one instance after most_recent_download variable
+            except:
+                try:
+                    global excel_file
+                    excel_file = xw.Book(save_folder + zip_filename[62:70] + ".xlsx")
+                    update_csv_data(zip_filename, csv_filename)
+                except:
+                    most_recent_download = find_most_recent_0000(most_recent_download, list_of_documents, current_document_position, zip_filename)
+                    remove(downloads_folder + zip_filename)
+            if most_recent_download != top_document:
+                csv_zip_details = download_csv_zip(most_recent_download, list_of_documents, list_of_links)
+            else:
+                pass
+                
+        time.sleep(900)
 
-        most_recent_download = temp_download   #Temporary DL variable becomes Most Recent Download
-        print(temp_download[58:] + " " + time.ctime(), end='\r')
-        webbrowser.open('http://mis.ercot.com' + (str(temp_download)))    #Downloads next SPP
-
-        update_csv_data()
-
-        last_save = open("testSPP.txt", "w")
-        last_save.write(most_recent_download)    #Update testSPP with new most_recent_download variable
-        last_save.close()
-
-    time.sleep(900)    #Wait 900 seconds to restart loop
+main()
